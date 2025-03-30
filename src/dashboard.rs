68a415c12d26cff3188fcfc70dcdd8660b17cbec
Tamo <tamo@meilisearch.com>
2025-03-30 03:49:12 +0200

    fix trunk by updating the template to the latest version of egui

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -12,7 +12,7 @@ impl Dashboard {
     pub fn new(reports: &[Report]) -> Self {
         let mut reports = reports.iter();
         let first_report = reports.next().unwrap().clone();
-        let maxi_report = reports.fold(first_report, |left, right| left.merge(&right));
+        let maxi_report = reports.fold(first_report, |left, right| left.merge(right));
 
         Self {
             maxi_report,
